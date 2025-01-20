3ff94ee690aaecdda6f6f68b997bea3326226f74
Isabel Veliz <iveliz21@apu.edu>
2025-01-19 16:59:05 -0800

    Fixed Bugs + Added Disable Hud
    I fixed the script Damage_Number so that it does not open to compiling error
    added the disabling of HUD
    noticed a problem with calling mulitple children in a parent. need to change the images to reflex multiple numbers not single digits

--- JAAVIgame/Assets/Scripts/Damage_Number.cs
+++ JAAVIgame/Assets/Scripts/Damage_Number.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Damage_Number : MonoBehaviour
 {
@@ -8,38 +9,41 @@ public class Damage_Number : MonoBehaviour
     [SerializeField] public static int damage = 0;
 
     // This is our HUD image UI Object
-    [SerializeField] public state Image hud;
+    [SerializeField] public static Image damNum;
+    [SerializeField] public static Image damNum2;
 
     //damage num sprites
 
-    [SerializeField] Sprite damage0tex
+    [SerializeField] Sprite damage0tex;
 
-    [SerializeField] Sprite damage1tex
+    [SerializeField] Sprite damage1tex;
 
-    [SerializeField] Sprite damage2tex
+    [SerializeField] Sprite damage2tex;
 
-    [SerializeField] Sprite damage3tex
+    [SerializeField] Sprite damage3tex;
 
-    [SerializeField] Sprite damage4tex
+    [SerializeField] Sprite damage4tex;
 
-    [SerializeField] Sprite damage5tex
+    [SerializeField] Sprite damage5tex;
 
-    [SerializeField] Sprite damage6tex
+    [SerializeField] Sprite damage6tex;
 
-    [SerializeField] Sprite damage7tex
+    [SerializeField] Sprite damage7tex;
 
-    [SerializeField] Sprite damage8tex
+    [SerializeField] Sprite damage8tex;
 
-    [SerializeField] Sprite damage9tex
+    [SerializeField] Sprite damage9tex;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        hud = gameObject,GetComponentInChildren<Image>();
+        damNum = gameObject.GetComponentInChildren<Image>();
+        damNum2 = gameObject.GetComponentInChildren<Image>();
 
         //if u wanna hide hud
-        //hud.emabled = false;
+        damNum.enabled = false;
+        damNum2.enabled = false;
 
         //set initial damagecount value
         damage = 0;
@@ -48,10 +52,12 @@ public class Damage_Number : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (damage == 1)
+        if (damage == 10)
         {
-            hud.sprite = damage1tex
-            //hud.enabled = true;
+            damNum2.sprite = damage1tex;
+            damNum2.enabled = true;
+            damNum.sprite = damage0tex;
+            damNum.enabled = true;
         }
     }
 }
