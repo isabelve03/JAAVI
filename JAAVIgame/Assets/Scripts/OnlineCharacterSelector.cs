57cabf5de364e832a3d7ac2190d740f74c48354e
Josiah-Emerson <102493086+Josiah-Emerson@users.noreply.github.com>
2025-04-23 02:55:02 -0700

    rpc changfe

--- JAAVIgame/Assets/Scripts/OnlineCharacterSelector.cs
+++ JAAVIgame/Assets/Scripts/OnlineCharacterSelector.cs
@@ -6,24 +6,18 @@ using FishNet.Managing;
 
 public class OnlineCharacterSelector : NetworkBehaviour
 {
+    private NetworkManager _networkManager;
     public override void OnStartClient()
     {
         base.OnStartClient();
+        _networkManager = FindObjectOfType<NetworkManager>();
     }
 
     [ServerRpc]
     public void ServerPlayerIsReady(bool isHost, bool ready)
     {
         Debug.Log("SERVER");
-        ClientPlayerIsReady(isHost, ready);
-    }
-
-
-    [ObserversRpc]
-    private void ClientPlayerIsReady(bool isHost, bool ready)
-    {
-        Debug.Log("CLIENT");
-        FindObjectOfType<NetworkManager>().GetComponent<LobbyManager>().PlayerReady(isHost, ready);
+        _networkManager.GetComponent<LobbyManager>().PlayerReady(isHost, ready);
     }
 
 }
