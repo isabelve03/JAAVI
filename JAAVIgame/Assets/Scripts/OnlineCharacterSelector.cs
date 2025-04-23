58150e66df6d02ef88010323d833dbf1e454d7d5
Josiah-Emerson <jemerson20@apu.edu>
2025-04-23 09:41:14 -0700

    Possible Fix for spawning having no data (SEE TODO in OnlineCharacterSelecor)

--- JAAVIgame/Assets/Scripts/OnlineCharacterSelector.cs
+++ JAAVIgame/Assets/Scripts/OnlineCharacterSelector.cs
@@ -6,6 +6,9 @@ using FishNet.Managing;
 
 public class OnlineCharacterSelector : NetworkBehaviour
 {
+    // TODO - Possible reason for not workking to spawn: 
+    // this netwrok manager (and where the prefabs are saved to) is found on start client
+    // try change this to find network manager within the server RPC to see if this ensure network manager/lobby manager are same for the spawn script
     private NetworkManager _networkManager;
     public override void OnStartClient()
     {
