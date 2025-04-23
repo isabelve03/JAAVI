43e74eb5b5c2ef63b8b08fc162ec9fae100f17de
Josiah-Emerson <102493086+Josiah-Emerson@users.noreply.github.com>
2025-04-22 20:49:40 -0700

    Removed some old stuff and started w new

--- JAAVIgame/Assets/Scripts/OnlineGameManager.cs
+++ JAAVIgame/Assets/Scripts/OnlineGameManager.cs
@@ -7,119 +7,59 @@ using UnityEngine;
 using FishNet.Managing;
 using Steamworks.Data;
 using FishNet.Managing.Scened;
+using UnityEngine.Timeline;
 
 public class OnlineGameManager : NetworkBehaviour
 {
-    public NetworkObject Player1 { get; private set; }
-    public NetworkObject Player2 { get; private set; }
-    private NetworkConnection _playerConnection1; // host client
-    private NetworkConnection _playerConnection2; // regular client
-    private int numReady = 0; // num people clicked start/ready button (used for both lobby and character select screens)
-    private int numPlayers = 0;
+    private int numReady = 0; // tracks number of players ready
+    private bool _leftLobbyScene = false; // changes to true when lobby scene is left
     private NetworkManager _networkManager;
     private SceneManager _sceneManager;
-    private CharacterSelectionManager _characterSelectionManager;
 
-    private void Awake()
+
+    public override void OnStartClient()
     {
-        if (InstanceFinder.IsServerStarted)
+        base.OnStartClient();
+
+        if (!IsClientInitialized)
         {
-            Debug.Log("Host");
+            Debug.LogWarning("Client not initialized, returning...");
+            return;
         }
-        else
-        {
-            Debug.Log("client");
-        }
+
         _networkManager = FindObjectOfType<NetworkManager>();
-        if(_networkManager == null )
+        if(_networkManager == null)
         {
-            Debug.LogError("Could not find network manager...");
+            Debug.LogWarning("Could not find Network Manager...");
         }
 
-        _characterSelectionManager = FindObjectOfType<CharacterSelectionManager>();
-        if(_characterSelectionManager == null)
+        _sceneManager = FindObjectOfType<SceneManager>();
+        if(_sceneManager == null)
         {
-            Debug.LogError("Could not find character selection manager...");
+            Debug.LogWarning("Could not find Scene Manager...");
         }
 
-        _sceneManager = _networkManager.SceneManager;
-        if( _sceneManager == null)
-        {
-            Debug.LogError("Could not find scene manager");
-        }
-
+        // subscribe to callbacks
         _sceneManager.OnClientLoadedStartScenes += SceneManager_OnClientLoadedStartScenes;
     }
 
     private void SceneManager_OnClientLoadedStartScenes(NetworkConnection conn, bool asServer)
     {
-        Debug.Log("Scene Loaded...");
-    }
-    public override void OnStartClient()
-    {
-        base.OnStartClient();
-        if (!IsClientInitialized)
+        if (!asServer)
             return;
 
-        ServerPlayerRegister(_characterSelectionManager.SelectedNetworkCharacter, InstanceFinder.IsServerStarted);
-    }
-
-    [ServerRpc]
-    public void ServerLobbyJoined()
-    {
-        numPlayers++;
-        if(numPlayers == 2)
+        // in lobby
+        if (!_leftLobbyScene)
         {
-            numPlayers = 0;
-            LoadScene("CharacterSelect");
-        }
-    }
-    [ServerRpc]
-    public void ServerReadyToStartGame(bool clicked)
-    {
-        // clicked will be false if 1st time clicking, else will be true
-        if (!clicked)
-        {
-            // increment if first time clicking ready/start
             numReady++;
-        }
-
-        if (numReady == 2)
-            LoadScene("TEST_ONLINE_BATTLE");
-    }
-
-    [ServerRpc]
-    private void LoadScene(string sceneName)
-    {
-        SceneLoadData sld = new SceneLoadData(sceneName);
-        sld.Options.AllowStacking = false; // replaces the existing scene
-        _sceneManager.LoadGlobalScenes(sld);
-    }
-    [ServerRpc (RequireOwnership = false)]
-    public void ServerPlayerRegister(NetworkObject player, bool isHost)
-    {
-        Debug.Log("SERVER: In ServerPlayerRegister rpc");
-        if (isHost)
-        {
-            Player1 = player;
-            numPlayers++;
-            _playerConnection1 = _networkManager.GetComponent<OnlinePlayerConnections>()._connectionHost;
-            Debug.Log($"Registered player 1 (host) as {Player1.name}");
-            Debug.Log($"Registered player 1's connection (host) as {_playerConnection1}");
-        }
-        else
-        {
-            Player2 = player;
-            numPlayers++;
-            _playerConnection2 = _networkManager.GetComponent<OnlinePlayerConnections>()._connectionClient;
-            Debug.Log($"Registered player 2 (client) as {Player2.name}");
-            Debug.Log($"Registered player 2's connection (client) as {_playerConnection2}");
-
+            Debug.Log($"Connection: {conn} is player #{numReady}");
         }
     }
 
     private void OnDestroy()
     {
+        // unsubscribe from callbacks
         _sceneManager.OnClientLoadedStartScenes -= SceneManager_OnClientLoadedStartScenes;
     }
 }
+
