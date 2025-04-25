817913c48d2228457f99b2f12337f6df0c6a501a
Josiah-Emerson <102493086+Josiah-Emerson@users.noreply.github.com>
2025-04-25 00:03:11 -0700

    SA

--- JAAVIgame/Assets/Scripts/OnlineCombat.cs
+++ JAAVIgame/Assets/Scripts/OnlineCombat.cs
@@ -84,9 +84,8 @@ public class OnlineCombat : NetworkBehaviour
         foreach(Collider2D collider in hitOpponnet)
         {
             cnt++;
-            Debug.Log($"Name: {collider.gameObject.name}");
-            Debug.Log($"If equal no .gameObject: {collider.gameObject.Equals(oppPlayer)}");
-            Debug.Log($"If equal with .gameObject: {collider.gameObject.Equals(oppPlayer.gameObject)}");
+            Debug.Log($"Name of collider {collider.gameObject.name}");
+            Debug.Log($"Name of opp {oppPlayer.name}");
             Debug.Log($"Collider {cnt}");
             if(collider.gameObject == oppPlayer.gameObject)
             {
