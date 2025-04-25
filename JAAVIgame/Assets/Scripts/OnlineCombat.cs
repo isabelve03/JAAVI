51b4917be4621b3c01e16752ab0f6c56d31a6cb4
Josiah-Emerson <102493086+Josiah-Emerson@users.noreply.github.com>
2025-04-24 21:58:38 -0700

    quick change

--- JAAVIgame/Assets/Scripts/OnlineCombat.cs
+++ JAAVIgame/Assets/Scripts/OnlineCombat.cs
@@ -78,7 +78,7 @@ public class OnlineCombat : NetworkBehaviour
                 Debug.Log("Found");
                 Debug.Log(oppConn);
                 Debug.Log(attackDamage);
-                t_Attack(oppConn, attackDamage);
+                t_Attack(ClientManager.Connection, attackDamage);
                 break;
             }
         }
