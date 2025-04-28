ccdb97b1fddaaab27f1726c73838e6b449a93d16
Josiah-Emerson <102493086+Josiah-Emerson@users.noreply.github.com>
2025-04-28 16:51:54 -0700

    return;

--- JAAVIgame/Assets/Scripts/OnlineGameManager.cs
+++ JAAVIgame/Assets/Scripts/OnlineGameManager.cs
@@ -25,6 +25,7 @@ public class OnlineGameManager : NetworkBehaviour
         base.OnStartClient();
 
         if (!InstanceFinder.IsServerStarted)
+            return;
         ServerSpawnDeathBarrier();
         ServerSpawnCharacters();
     }
