3c5112f30cc08e5e54bbf2e49aa8d83941e95355
Josiah-Emerson <102493086+Josiah-Emerson@users.noreply.github.com>
2025-04-28 14:31:51 -0700

    Return to menu func

--- JAAVIgame/Assets/Scripts/OnlineVictoryScreen.cs
+++ JAAVIgame/Assets/Scripts/OnlineVictoryScreen.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OnlineVictoryScreen : MonoBehaviour
 {
     [SerializeField] private GameObject _victoryScreen;
+    [SerializeField] private Button _quitButton;
 
     private void Awake()
     {
         // Initially make sure the victory screen is hidden
         _victoryScreen.SetActive(false);
+        _quitButton.gameObject.SetActive(false);
     }
 
     public void ShowVictoryScreen()
     {
         _victoryScreen.SetActive(true);
+        _quitButton.gameObject.SetActive(true); 
     }
 }
